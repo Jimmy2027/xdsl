78827974de944c5c7ba058618cce5f3304a4496d
Hendrik Klug <hendrik.klug@gmail.com>
2025-05-19 18:03:43 +0200

    add emitC dialect

--- xdsl/dialects/emitc.py
+++ xdsl/dialects/emitc.py
@@ -373,9 +373,289 @@ def is_supported_emitc_type(type_attr: Attribute) -> bool:
             return False
 
 
+@irdl_op_definition
+class EmitC_AddOp(IRDLOperation):
+    """Addition operation"""
+
+    name = "emitc.add"
+    assembly_format = """operands attr-dict `:` functional-type(operands, results)"""
+    lhs = operand_def(AnyAttr())
+    rhs = operand_def(AnyAttr())
+    v1 = result_def(AnyAttr())
+
+
+@irdl_op_definition
+class EmitC_ApplyOp(IRDLOperation):
+    """Apply operation"""
+
+    name = "emitc.apply"
+    assembly_format = """
+        $applicableOperator `(` $operand `)` attr-dict `:` functional-type($operand, results)
+      """
+    applicableOperator = prop_def(AnyAttr())
+    operand = operand_def(AnyOf((AnyAttr(), BaseAttr(EmitC_LValueType))))
+    result = result_def(AnyAttr())
+
+
+@irdl_op_definition
+class EmitC_AssignOp(IRDLOperation):
+    """Assign operation"""
+
+    name = "emitc.assign"
+    var = prop_def(AnyAttr())
+    value = operand_def(AnyAttr())
+
+
+@irdl_op_definition
+class EmitC_BitwiseAndOp(IRDLOperation):
+    name = "emitc.bitwise_and"
+
+
+@irdl_op_definition
+class EmitC_BitwiseLeftShiftOp(IRDLOperation):
+    name = "emitc.bitwise_left_shift"
+
+
+@irdl_op_definition
+class EmitC_BitwiseNotOp(IRDLOperation):
+    name = "emitc.bitwise_not"
+
+
+@irdl_op_definition
+class EmitC_BitwiseOrOp(IRDLOperation):
+    name = "emitc.bitwise_or"
+
+
+@irdl_op_definition
+class EmitC_BitwiseRightShiftOp(IRDLOperation):
+    name = "emitc.bitwise_right_shift"
+
+
+@irdl_op_definition
+class EmitC_BitwiseXorOp(IRDLOperation):
+    name = "emitc.bitwise_xor"
+
+
+@irdl_op_definition
+class EmitC_CallOp(IRDLOperation):
+    name = "emitc.call"
+
+
+@irdl_op_definition
+class EmitC_CallOpaqueOp(IRDLOperation):
+    name = "emitc.call_opaque"
+
+
+@irdl_op_definition
+class EmitC_CastOp(IRDLOperation):
+    name = "emitc.cast"
+
+
+@irdl_op_definition
+class EmitC_CmpOp(IRDLOperation):
+    name = "emitc.cmp"
+
+
+@irdl_op_definition
+class EmitC_ConditionalOp(IRDLOperation):
+    name = "emitc.conditional"
+
+
+@irdl_op_definition
+class EmitC_ConstantOp(IRDLOperation):
+    name = "emitc.constant"
+
+
+@irdl_op_definition
+class EmitC_DeclareFuncOp(IRDLOperation):
+    name = "emitc.declare_func"
+
+
+@irdl_op_definition
+class EmitC_DivOp(IRDLOperation):
+    name = "emitc.div"
+
+
+@irdl_op_definition
+class EmitC_ExpressionOp(IRDLOperation):
+    name = "emitc.expression"
+
+
+@irdl_op_definition
+class EmitC_FileOp(IRDLOperation):
+    name = "emitc.file"
+
+
+@irdl_op_definition
+class EmitC_ForOp(IRDLOperation):
+    name = "emitc.for"
+
+
+@irdl_op_definition
+class EmitC_FuncOp(IRDLOperation):
+    name = "emitc.func"
+
+
+@irdl_op_definition
+class EmitC_GetGlobalOp(IRDLOperation):
+    name = "emitc.get_global"
+
+
+@irdl_op_definition
+class EmitC_GlobalOp(IRDLOperation):
+    name = "emitc.global"
+    sym_name: SymbolNameAttr = prop_def(SymbolNameAttr)
+    type: EmitC_ArrayType = prop_def(EmitC_ArrayType)
+    initial_value: Attribute | None = opt_prop_def(Attribute)
+
+
+@irdl_op_definition
+class EmitC_IfOp(IRDLOperation):
+    name = "emitc.if"
+
+
+@irdl_op_definition
+class EmitC_IncludeOp(IRDLOperation):
+    name = "emitc.include"
+
+
+@irdl_op_definition
+class EmitC_LiteralOp(IRDLOperation):
+    name = "emitc.literal"
+
+
+@irdl_op_definition
+class EmitC_LoadOp(IRDLOperation):
+    name = "emitc.load"
+
+
+@irdl_op_definition
+class EmitC_LogicalAndOp(IRDLOperation):
+    name = "emitc.logical_and"
+
+
+@irdl_op_definition
+class EmitC_LogicalNotOp(IRDLOperation):
+    name = "emitc.logical_not"
+
+
+@irdl_op_definition
+class EmitC_LogicalOrOp(IRDLOperation):
+    name = "emitc.logical_or"
+
+
+@irdl_op_definition
+class EmitC_MemberOfPtrOp(IRDLOperation):
+    name = "emitc.member_of_ptr"
+
+
+@irdl_op_definition
+class EmitC_MemberOp(IRDLOperation):
+    name = "emitc.member"
+
+
+@irdl_op_definition
+class EmitC_MulOp(IRDLOperation):
+    name = "emitc.mul"
+
+
+@irdl_op_definition
+class EmitC_RemOp(IRDLOperation):
+    name = "emitc.rem"
+
+
+@irdl_op_definition
+class EmitC_ReturnOp(IRDLOperation):
+    name = "emitc.return"
+
+
+@irdl_op_definition
+class EmitC_SubOp(IRDLOperation):
+    name = "emitc.sub"
+
+
+@irdl_op_definition
+class EmitC_SubscriptOp(IRDLOperation):
+    name = "emitc.subscript"
+
+
+@irdl_op_definition
+class EmitC_SwitchOp(IRDLOperation):
+    name = "emitc.switch"
+
+
+@irdl_op_definition
+class EmitC_UnaryMinusOp(IRDLOperation):
+    name = "emitc.unary_minus"
+
+
+@irdl_op_definition
+class EmitC_UnaryPlusOp(IRDLOperation):
+    name = "emitc.unary_plus"
+
+
+@irdl_op_definition
+class EmitC_VariableOp(IRDLOperation):
+    name = "emitc.variable"
+
+
+@irdl_op_definition
+class EmitC_VerbatimOp(IRDLOperation):
+    name = "emitc.verbatim"
+
+
+@irdl_op_definition
+class EmitC_YieldOp(IRDLOperation):
+    name = "emitc.yield"
+
+
 EmitC = Dialect(
     "emitc",
-    [],
+    [
+        EmitC_AddOp,
+        EmitC_ApplyOp,
+        EmitC_AssignOp,
+        EmitC_BitwiseAndOp,
+        EmitC_BitwiseLeftShiftOp,
+        EmitC_BitwiseNotOp,
+        EmitC_BitwiseOrOp,
+        EmitC_BitwiseRightShiftOp,
+        EmitC_BitwiseXorOp,
+        EmitC_CallOp,
+        EmitC_CallOpaqueOp,
+        EmitC_CastOp,
+        EmitC_CmpOp,
+        EmitC_ConditionalOp,
+        EmitC_ConstantOp,
+        EmitC_DeclareFuncOp,
+        EmitC_DivOp,
+        EmitC_ExpressionOp,
+        EmitC_FileOp,
+        EmitC_ForOp,
+        EmitC_FuncOp,
+        EmitC_GetGlobalOp,
+        EmitC_GlobalOp,
+        EmitC_IfOp,
+        EmitC_IncludeOp,
+        EmitC_LiteralOp,
+        EmitC_LoadOp,
+        EmitC_LogicalAndOp,
+        EmitC_LogicalNotOp,
+        EmitC_LogicalOrOp,
+        EmitC_MemberOfPtrOp,
+        EmitC_MemberOp,
+        EmitC_MulOp,
+        EmitC_RemOp,
+        EmitC_ReturnOp,
+        EmitC_SubOp,
+        EmitC_SubscriptOp,
+        EmitC_SwitchOp,
+        EmitC_UnaryMinusOp,
+        EmitC_UnaryPlusOp,
+        EmitC_VariableOp,
+        EmitC_VerbatimOp,
+        EmitC_YieldOp,
+    ],
     [
         EmitC_ArrayType,
         EmitC_LValueType,
